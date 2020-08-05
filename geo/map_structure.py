6f8ffc2468525f3b47cde27ba848706d9178beeb
tzufgoogle <tzuf@google.com>
2020-08-05 07:56:30 +0000

    add streets to graph

--- geo/map_structure.py
+++ geo/map_structure.py
@@ -28,7 +28,7 @@ class Map:
             self.place_polygon = shapely.geometry.box(
                 miny=40.425, minx=-80.035, maxy=40.460, maxx=-79.930, ccw=True)
 
-        self.poi = self.__get_poi()
+        self.poi, self.streets = self.__get_poi()
         self.__create_graph()
 
     def __get_poi(self):
@@ -37,20 +37,21 @@ class Map:
 
         osm_poi = ox.pois.pois_from_polygon(self.place_polygon, tags=tags)
         osm_poi = osm_poi[osm_poi['name'].notnull()]
-        osm_poi = osm_poi[osm_poi['highway'].isnull()]
-        return osm_poi
+        osm_poi_no_streets = osm_poi[osm_poi['highway'].isnull()]
+        osm_poi_streets = osm_poi[osm_poi['highway'].notnull()]
 
-    def cellid_from_polygon(self, polygon, level):
+        return osm_poi, osm_poi_streets
+
+
+    def cellid_from_s2shape(self, s2shape, level):
         # OpenStreetMaps Ways type to cellids
 
         coverer = s2.S2RegionCoverer()
         coverer.set_min_level(level)
         coverer.set_max_level(level)
         coverer.set_max_cells(100)
-        covering = coverer.GetCovering(polygon)
+        covering = coverer.GetCovering(s2shape)
 
-        for cellid in covering:
-            assert polygon.Intersects(s2.S2Polygon(s2.S2Cell(cellid)))
         return covering
 
     def latlng_from_point(self, point):
@@ -90,6 +91,19 @@ class Map:
         s2point_list = list(map(self.s2point_from_coord_xy, list_coords))
         s2point_list = s2point_list[::-1]  # Counterclockwise
         return s2.S2Polygon(s2.S2Loop(s2point_list))
+    
+    def s2polygon_from_shapely_polyline(self, shapely_polygon):
+
+        list_coords = list(shapely_polygon.exterior.coords)
+
+        list_ll = []
+        for lat, lng in list_coords:
+            list_ll.append(s2.S2LatLng.FromDegrees(lat, lng))
+
+        line = s2.S2Polyline()
+        line.InitFromS2LatLngs(list_ll)
+
+        return line
 
     def cellid_from_geometry(self, geo, level):
         assert isinstance(geo, Point) or isinstance(geo, Polygon), type(geo)
@@ -100,26 +114,51 @@ class Map:
             s2_polygon = self.s2polygon_from_shapely_polygon(geo)
             if s2_polygon is None:
                 return None
-            return self.cellid_from_polygon(s2_polygon, level)
+            return self.cellid_from_s2shape(s2_polygon, level)
+
+
+    def cellid_from_geometry_streets(self, geo, level):
+        assert isinstance(geo, Point) or isinstance(geo, Polygon), type(geo)
+        if isinstance(geo, Point):
+            s2_point = self.s2point_from_shapely_point(geo)
+            return self.cellid_from_point(s2_point, level)
+        else:
+            s2_polyline = self.s2polygon_from_shapely_polyline(geo)
+            return self.cellid_from_s2shape(s2_polyline, level)
 
     def add_poi_to_graph(self, row):
         cells = row.cellids
         poi = row.osmid
         self.graph.add_poi(cells, poi)
+    
+    def add_street_to_graph(self, row):
+        cells = row.cellids
+        street = row.osmid
+        self.graph.add_street(cells, street)
 
     def __create_graph(self):
         level = 18
 
-        # Get cellids.
+        # Get cellids for POI.
         self.poi['cellids'] = self.poi['geometry'].apply(
             self.cellid_from_geometry, args=[level])
         self.poi = self.poi[self.poi['cellids'].notnull()]
 
+        # Get cellids for streets
+        self.streets['cellids'] = self.streets['geometry'].apply(
+            self.cellid_from_geometry_streets, args=[level])
+        self.streets = self.streets[self.streets['cellids'].notnull()]
+
+
         # Create graph.
         self.graph = Graph()
 
-        # Add Poi to graph
+        # Add POI to graph
         self.poi[['cellids', 'osmid']].apply(self.add_poi_to_graph, axis=1)
+
+
+        # Add street to graph
+        self.streets[['cellids', 'osmid']].apply(self.add_street_to_graph, axis=1)
 
         '''
         TODO: 
@@ -131,3 +170,4 @@ class Map:
 if __name__ == "__main__":
 
     pittsburgh_map = Map("Pittsburgh")
+    print ("END")
