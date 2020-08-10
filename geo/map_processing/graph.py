ae2bea503603a935050ffe6b0564c5d6d4a967be
tzufgoogle <tzuf@google.com>
2020-08-10 15:05:09 +0000

    fixed all bugs, added comments, added init files

--- geo/map_processing/graph.py
+++ geo/map_processing/graph.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from s2geometry import pywraps2 as s2
 import networkx as nx
 from typing import Dict, Tuple, Sequence
@@ -28,11 +29,12 @@ NUM_POS_BITS = 2 * MAX_LEVEL + 1
 
 # The begining of the position bits minus the two unique cell bits of first
 # level.
-START_BITS = NUM_POS_BITS - 2
+START_BIT = NUM_POS_BITS - 2
 
 
 class MapNode:
 
+    # TODO(jasonbaldridge): Init using builder pattern.
     def __init__(self, parent):
         self.parent = parent
         self.neighbors = [None, None, None, None, None, None, None, None]
@@ -40,23 +42,23 @@ class MapNode:
         self.poi = []
         self.streets = []
         self.level = 0
-        if self.parent:
+        if self.parent is not None:
             self.level = self.parent.level + 1
 
 
-class Graph:
+class MapGraph:
 
     def __init__(self):
         self.faces = tuple([MapNode(None) for x in range(0, NUM_FACES)])
 
-    def get_cell_neighbors(self, cell: s2.S2Cell) -> Sequence:
-        '''Get eight s2cell neighbors for a given cell. 
+    def get_cell_neighbors(self, cell: s2.S2Cell) -> Sequence[MapNode]:
+        '''Get eight S2Cell neighbors for a given cell. 
         Arguments:
-            cell(S2Cell): an S2Cell.
+            cell(S2Cell): The relative S2Cell to which the S2Cell neighbors will refer to.
         Returns:
-            A sequence of eight S2Cell2.
+            A sequence of eight S2Cell2 neighbors.
         '''
-        
+
         four_neighbors = cell.GetEdgeNeighbors()
         eight_neighbors = four_neighbors + [
             four_neighbors[0].next(), four_neighbors[3].next(),
@@ -66,11 +68,12 @@ class Graph:
         eight_neighbors_nodes = [self.search(cell) for cell in eight_neighbors]
         return eight_neighbors_nodes
 
-    def add_street(self, cells: Sequence, osmid_of_street: int):
+    def add_street(self, cells: Sequence[s2.S2Cell], osmid_of_street: int):
         '''Add a street POI to multiple cells. 
         Arguments:
-            cells(sequence): a sequence of s2cells.
-            osmid_of_street(int): an osmid of the street.
+            cells(sequence): The S2Cells to which the street is to be added.
+            osmid_of_street(int): The osmid of the street that is to be added to
+            the S2Cells.
         '''
 
         for cell in cells:
@@ -80,31 +83,29 @@ class Graph:
             # For each level 2 unique bits (=4 children) are reserved.
             last_bit = NUM_POS_BITS - 2 * cell.level()
 
-            cellid = cell.id()
-
-            for i in range(START_BITS, last_bit - 1, -2):
-                # Get value of unique bites in the current level.
-                value_unique_bites = (cellid >> i) & 3
+            for i in range(START_BIT, last_bit - 1, -2):
+                # Get value of unique bits in the current level.
+                value_unique_bits = (cell.id >> i) & 3
 
                 # Check if the child node exists and if not add a child node.
-                if not curr_node.children[value_unique_bites]:
-                    curr_node.children[value_unique_bites] = MapNode(curr_node)
+                if not curr_node.children[value_unique_bits]:
+                    curr_node.children[value_unique_bits] = MapNode(curr_node)
 
                     # Connect the cell to its neighbors.
                     curr_node.neighbors = self.get_cell_neighbors(cell)
 
                 # Descend one level by changing th current node to be the child
                 # node.
-                curr_node = curr_node.children[value_unique_bites]
+                curr_node = curr_node.children[value_unique_bits]
 
             # Connect the cell to the street.
             curr_node.streets.append(osmid_of_street)
 
-    def add_poi(self, cells: Sequence, poi: int):
+    def add_poi(self, cells: Sequence[s2.S2Cell], poi: int):
         '''Add a POI to multiple cells. 
         Arguments:
-            cells(sequence): a sequence of s2cells.
-            poi(int): an osmid of the POI.
+            cells(sequence): The S2Cells to which the POI will be added.
+            poi(int): an osmid of the POI that will be added to the S2Cells.
         '''
 
         for cell in cells:
@@ -114,22 +115,20 @@ class Graph:
             # For each level 2 unique bits (=4 children) are reserved.
             last_bit = NUM_POS_BITS - 2 * cell.level()
 
-            cellid = cell.id()
-
-            for i in range(START_BITS, last_bit - 1, -2):
-                # Get value of unique bites in the current level.
-                value_unique_bites = (cellid >> i) & 3
+            for i in range(START_BIT, last_bit - 1, -2):
+                # Get value of unique bits in the current level.
+                value_unique_bits = (cell.id() >> i) & 3
 
                 # Check if the child node exists and if not add a child node.
-                if not curr_node.children[value_unique_bites]:
-                    curr_node.children[value_unique_bites] = MapNode(curr_node)
+                if not curr_node.children[value_unique_bits]:
+                    curr_node.children[value_unique_bits] = MapNode(curr_node)
 
                     # Connect the cell to its neighbors.
                     curr_node.neighbors = self.get_cell_neighbors(cell)
 
                 # Descend one level by changing th current node to be the child
                 # node.
-                curr_node = curr_node.children[value_unique_bites]
+                curr_node = curr_node.children[value_unique_bits]
 
             # Connect the cell to the POI.
             curr_node.poi.append(poi)
@@ -137,9 +136,9 @@ class Graph:
     def search(self, cell: s2.S2Cell) -> Optional[MapNode]:
         '''Get all POI for a specific cell. 
         Arguments:
-            cell(S2Cell): an s2cell.
+            cell(S2Cell): The S2Cell to search for in the graph.
         Returns:
-            The Node in the graph.
+            The Node in the graph that contains the S2Cell.
         '''
 
         curr_node = self.faces[cell.face()]
@@ -148,12 +147,10 @@ class Graph:
         # 2 unique bits (=4 children) are reserved.
         last_bit = NUM_POS_BITS - 2 * cell.level()
 
-        cellid = cell.id()
+        for i in range(START_BIT, last_bit - 1, -2):
+            value_unique_bits = (cell.id() >> i) & 3
 
-        for i in range(START_BITS, last_bit - 1, -2):
-            value_unique_bites = (cellid >> i) & 3
-
-            curr_node = curr_node.children[value_unique_bites]
+            curr_node = curr_node.children[value_unique_bits]
 
             if not curr_node:
                 return None
