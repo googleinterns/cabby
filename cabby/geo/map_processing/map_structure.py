6c93c718a6f44f01f2ea38263a223dbea4eb2eef
tzufgoogle <tzuf@google.com>
2020-08-16 08:08:39 +0000

    fixed two bugs: (1) poi->street (2).id->id()

--- cabby/geo/map_processing/map_structure.py
+++ cabby/geo/map_processing/map_structure.py
@@ -67,7 +67,7 @@ class Map:
             x, level) if isinstance(x, Point) else geo_utils.cellid_from_polygon(x, level))
 
         # Get cellids for streets.
-        self.streets['cellids'] = self.poi['geometry'].apply(lambda x: geo_utils.cellid_from_point(
+        self.streets['cellids'] = self.streets['geometry'].apply(lambda x: geo_utils.cellid_from_point(
             x, level) if isinstance(x, Point) else geo_utils.cellid_from_polyline(x, level))
 
         # Filter out entities that we didn't mange to get cellids covering.
