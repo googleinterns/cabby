84883d59303889348e0e784a75f7af965e0bd8fc
tzufgoogle <tzuf@google.com>
2020-08-20 13:06:59 +0000

    added bash file for filtering and spliting the data

---
+++ cabby/data/split.sh
@@ -0,0 +1,12 @@
+# Filter entities that do not have text.
+jq -c 'select(.text != "")'  Pittsburgh.json
+
+
+# Split the file into a train(%80), dev(10%) and test(%10) sets. 
+jq -c -s 'sort_by(.qid)[:length*8/10]|.[]'  /mnt/hackney/data/cabby/wikigeo/wikigeo_pittsburgh.json > /mnt/hackney/data/cabby/wikigeo/wikigeo_pittsburgh_train.json 
+jq -c -s 'sort_by(.qid)[length*8/10+1:length*9/10]|.[]'  /mnt/hackney/data/cabby/wikigeo/wikigeo_pittsburgh.json > /mnt/hackney/data/cabby/wikigeo/wikigeo_pittsburgh_dev.json 
+jq -c -s 'sort_by(.qid)[length*9/10:]|.[]'  /mnt/hackney/data/cabby/wikigeo/wikigeo_pittsburgh.json > /mnt/hackney/data/cabby/wikigeo/wikigeo_pittsburgh_test.json 
+jq -c -s 'sort_by(.qid)[:length*8/10]|.[]'  /mnt/hackney/data/cabby/wikigeo/wikigeo_manhattan.json > /mnt/hackney/data/cabby/wikigeo/wikigeo_manhattan_train.json 
+jq -c -s 'sort_by(.qid)[length*8/10+1:length*9/10]|.[]'  /mnt/hackney/data/cabby/wikigeo/wikigeo_manhattan.json > /mnt/hackney/data/cabby/wikigeo/wikigeo_manhattan_dev.json 
+jq -c -s 'sort_by(.qid)[length*9/10:]|.[]'  /mnt/hackney/data/cabby/wikigeo/wikigeo_manhattan.json > /mnt/hackney/data/cabby/wikigeo/wikigeo_manhattan_test.json 
+
