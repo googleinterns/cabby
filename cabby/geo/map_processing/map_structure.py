7c67948e761a45f89a762026830ee2b7fe8258d5
tzufgoogle <tzuf@google.com>
2020-09-03 12:49:54 +0000

    removed the hard-coded reference to data and made some other small changes

--- cabby/geo/map_processing/map_structure.py
+++ cabby/geo/map_processing/map_structure.py
@@ -34,7 +34,7 @@ from cabby.geo.map_processing import graph
 class Map:
 
     def __init__(self, map_name: Text, level: int, load_directory: Text = None):
-        assert map_name == "Manhattan" or map_name == "Pittsburgh"
+        assert map_name == "Manhattan" or map_name == "Pittsburgh" or map_name == "Bologna"
         self.map_name = map_name
         self.s2_graph = None
 
@@ -43,9 +43,13 @@ class Map:
                 'POLYGON ((-73.9455846946375 40.7711351085905,-73.9841893202025 40.7873649535321,-73.9976322499213 40.7733311258037,-74.0035177432988 40.7642404854275,-74.0097394992375 40.7563218869601,-74.01237903206 40.741427380319,-74.0159612551762 40.7237048027967,-74.0199205544099 40.7110727528606,-74.0203570671504 40.7073623945662,-74.0188292725586 40.7010329598287,-74.0087894795267 40.7003781907179,-73.9976584046436 40.707144138196,-73.9767057930988 40.7104179837498,-73.9695033328803 40.730061057073,-73.9736502039152 40.7366087481808,-73.968412051029 40.7433746956588,-73.968412051029 40.7433746956588,-73.9455846946375 40.7711351085905))'
             )
 
-        else:  # Pittsburgh.
+        elif map_name == "Pittsburgh":  
             self.polygon_area = box(
                 miny=40.425, minx=-80.035, maxy=40.460, maxx=-79.930, ccw=True)
+        else: # Bologna.
+            self.polygon_area = box(
+                miny=44.4902, minx=11.3333, maxy=44.5000, maxx=11.3564, ccw=True)
+
 
         if load_directory is None:
             self.poi, self.streets = self.get_poi()
@@ -152,7 +156,7 @@ class Map:
         # Load POI.
         base_filename = self.map_name.lower()+"_poi"
         path = os.path.join(dir_name, base_filename + ".pkl")
-        assert os.path.exists(path), "path {0} already doesn't exists".format(path)
+        assert os.path.exists(path), "path {0} doesn't exists. Current directory {1}.".format(path, os.getcwd())
         poi_pandas = pd.read_pickle(path)
         poi_pandas['cellids'] = poi_pandas['cellids'].apply(
             lambda x: util.s2cells_from_cellids(x))
@@ -161,7 +165,7 @@ class Map:
         # Load streets.
         base_filename = self.map_name.lower() + "_streets"
         path = os.path.join(dir_name, base_filename + ".pkl")
-        assert os.path.exists(path), "path {0} already doesn't exists".format(path)
+        assert os.path.exists(path), "path {0} doesn't exists.".format(path)
         streets_pandas = pd.read_pickle(path)
         streets_pandas['cellids'] = streets_pandas['cellids'].apply(
             lambda x: util.s2cells_from_cellids(x))
@@ -170,11 +174,11 @@ class Map:
         # Load graph.
         base_filename = self.map_name.lower() + "_graph"
         path = os.path.join(dir_name, base_filename + ".gpickle")
-        assert os.path.exists(path), "path {0} already doesn't exists".format(path)
+        assert os.path.exists(path), "path {0} doesn't exists.".format(path)
         self.nx_graph = nx.read_gpickle(path)
 
         # Load nodes.
         base_filename = self.map_name.lower() + "_nodes"
         path = os.path.join(dir_name, base_filename + ".geojson")
-        assert os.path.exists(path), "path {0} already doesn't exists".format(path)
+        assert os.path.exists(path), "path {0} doesn't exists.".format(path)
         self.nodes = gpd.read_file(path, driver='GeoJSON')
