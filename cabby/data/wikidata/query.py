9eb93e31ffafcedca668efa06d1007a6bb039523
tzuf <tzuf@google.com>
2020-09-21 13:03:19 +0000

    changed switer param

--- cabby/data/wikidata/query.py
+++ cabby/data/wikidata/query.py
@@ -15,6 +15,7 @@
 '''Library to support Wikidata geographic queries from https://query.wikidata.org/sparql.'''
 
 import sys
+import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, JSON
 from typing import Dict, Tuple, Sequence, Text
 
@@ -234,6 +235,45 @@ _PITTSBURGH_QUERY = ["""SELECT ?place ?placeLabel ?wikipediaUrl
           GROUP BY ?place ?placeLabel ?wikipediaUrl 
         """]
 
+_PITTSBURGH_RELATION_QUERY = ["""SELECT ?place ?placeLabel ?p ?propLabel ?instance ?instanceLabel
+          WHERE 
+          {
+          {
+            ?place ?p ?instance.
+            FILTER (?p IN (wdt:P31,
+                           wdt:P5353,
+                           wdt:P2012,
+                           wdt:P361,
+                           wdt:P149,
+                           wdt:P84,
+                           wdt:P138,
+                           wdt:P112,
+                           wdt:P1435,
+                           wdt:P1640,
+                           wdt:P463,
+                           wdt:P355,
+                           wdt:P527,
+                           wdt:P140) )
+            # SERVICE wikibase:label { bd:serviceParam wikibase:language "en". } 
+              ?prop wikibase:directClaim ?p .
+            ?wikipediaUrl schema:about ?place. 
+            ?wikipediaUrl schema:isPartOf <https://en.wikipedia.org/>. 
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+                        ?instance rdfs:label ?instanceLabel.  filter(lang(?instanceLabel) = "en").
+
+            SERVICE wikibase:box {
+            ?place wdt:P625 ?location .
+            bd:serviceParam wikibase:cornerWest "Point(-80.035,40.425)"^^geo:wktLiteral .
+            bd:serviceParam wikibase:cornerEast "Point(-79.930,40.460)"^^geo:wktLiteral .
+            }
+          }
+          FILTER (?instance  not in
+          (wd:Q34442,wd:Q12042110,wd:Q124757,wd:Q79007,wd:Q18340514,wd:Q537127,wd:Q1311958,wd:Q124757,
+          wd:Q25917154,  wd:Q1243306, wd:Q1570262, wd:Q811683,
+          wd:Q744913, wd:Q186117, wd:Q3298291) )
+          }
+        """]
+
 _DC_QUERY = ["""SELECT ?place ?placeLabel ?wikipediaUrl
            ( GROUP_CONCAT ( DISTINCT ?instanceLabel; separator="; " ) AS ?instance )
           (GROUP_CONCAT(DISTINCT?location;separator=", ") AS ?point)
@@ -308,6 +348,31 @@ def get_geofenced_wikidata_items(region: Text) -> Sequence[Dict]:
     raise ValueError(f"{region} is not a supported region.")
 
   return query_api(query)
+
+
+def get_geofenced_wikidata_relations(region: Text) -> Sequence[Dict]:
+  '''Get Wikidata relations for a specific area.
+  Arguments:
+    region(Text): The area to query the Wikidata items.
+  Returns:
+    The Wikidata items, and certain relations to other Wikidata items.
+  '''
+  if region == "Pittsburgh":
+    query = _PITTSBURGH_RELATION_QUERY
+
+  elif region == "Manhattan":
+    raise NotImplementedError(f"{region} is not an implemented region.")
+  
+  elif region == "D.C":
+    raise NotImplementedError(f"{region} is not an implemented region.")
+    
+  else:
+    raise ValueError(f"{region} is not a supported region.")
+
+  query_result = query_api(query)
+  result_df = pd.DataFrame([{k: v['value'] for k, v in x.items()} for x in query_result])
+  return result_df
+
 
 
 def get_geofenced_wikidata_items_by_qid(qid: Text) -> Sequence[Dict]:
