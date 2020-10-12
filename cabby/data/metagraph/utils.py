46eef5dd199c3962e503895e5e9190235502f75a
jpalowitch <palowitch@google.com>
2020-10-12 22:39:50 +0000

    Fix graph utils bug.

--- cabby/data/metagraph/utils.py
+++ cabby/data/metagraph/utils.py
@@ -81,7 +81,7 @@ def update_osm_map(osm_map: Map,
           continue
       already_added.add(row.place)
       osmid = hash(''.join(list(row.values)))
-      wd_query = query.get_geofenced_wikidata_items_by_qid(row.place)
+      wd_query = query.get_place_location_points_from_qid(row.place)
       new_df = pd.DataFrame(data={
           'name': row.placeLabel,
           'geometry': [util.point_str_to_shapely_point(wd_query[0]['point']['value'])],
