d77e4269b77b1e3832fb9550c4e9fa1f67d295e2
Jason Baldridge <jasonbaldridge@google.com>
2020-10-15 11:39:50 +0000

    Changed region definitions to be in dictionary and then the other variables are populated from that and get_region() just looks up in the dictionary and tries to get the polygon given the region name.

--- cabby/geo/regions.py
+++ cabby/geo/regions.py
@@ -11,36 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+'''Defines supported regions and their corresponding polygons.'''
 
-'''Defines the regions their corresponding polygons, for which the models, 
-data and maps are restricted to.'''
-
-from typing import Dict, Tuple, Sequence, Text, Optional, List, Any
+from typing import Dict
 
 from shapely.geometry import box
+from shapely.geometry.polygon import Polygon
 from shapely import wkt
-from shapely.geometry.point import Point
 
+REGION_POLYGONS: Dict[str, Polygon] = {
+    'Manhattan': wkt.loads(
+        ('POLYGON ((-73.9455846946375 40.7711351085905,-73.9841893202025 40.7873649535321,-73.9976322499213 40.7733311258037,-74.0035177432988 40.7642404854275,-74.0097394992375 40.7563218869601,-74.01237903206 40.741427380319,-74.0159612551762 40.7237048027967,-74.0199205544099 40.7110727528606,-74.0203570671504 40.7073623945662,-74.0188292725586 40.7010329598287,-74.0087894795267 40.7003781907179,-73.9976584046436 40.707144138196,-73.9767057930988 40.7104179837498,-73.9695033328803 40.730061057073,-73.9736502039152 40.7366087481808,-73.968412051029 40.7433746956588,-73.968412051029 40.7433746956588,-73.9455846946375 40.7711351085905))'
+    ),
+    'Pittsburgh': box(
+      miny=40.425, minx=-80.035, maxy=40.460, maxx=-79.930, ccw=True),
+    'UTAustin': box(minx=-97.7416, miny=30.2825, maxx=-97.7347, maxy=30.2877),
+    'DC': box(minx=-77.02767, miny=38.96608, maxx=-77.02704, maxy=38.9686)
+}
 
-ALLOWED_REGIONS = ["Manhattan", "Pittsburgh", "DC", "Pittsburgh_small"]
-TEXT_ALLOWED_REGIONS = "Map areas: Manhattan, Pittsburgh, DC. or Pittsburgh_small."
+ALLOWED_REGIONS = list(REGION_POLYGONS.keys())
+TEXT_ALLOWED_REGIONS = "Supported regions: " + ', '.join(ALLOWED_REGIONS)
 
-def get_region(region: Text):
-  assert (region in ALLOWED_REGIONS)
-  if region == "Manhattan":
-    polygon_area = wkt.loads(
-        'POLYGON ((-73.9455846946375 40.7711351085905,-73.9841893202025 40.7873649535321,-73.9976322499213 40.7733311258037,-74.0035177432988 40.7642404854275,-74.0097394992375 40.7563218869601,-74.01237903206 40.741427380319,-74.0159612551762 40.7237048027967,-74.0199205544099 40.7110727528606,-74.0203570671504 40.7073623945662,-74.0188292725586 40.7010329598287,-74.0087894795267 40.7003781907179,-73.9976584046436 40.707144138196,-73.9767057930988 40.7104179837498,-73.9695033328803 40.730061057073,-73.9736502039152 40.7366087481808,-73.968412051029 40.7433746956588,-73.968412051029 40.7433746956588,-73.9455846946375 40.7711351085905))'
-      )
-  elif region == "Pittsburgh":
-    polygon_area = box(
-            miny=40.425, minx=-80.035, maxy=40.460, maxx=-79.930,
-            ccw=True)
-  elif region == "DC": 
-    polygon_area = box(minx=-77.02767, 
-                       miny=38.96608, 
-                       maxx=-77.02704, 
-                       maxy=38.9686)
-  else: # Pittsburgh_small
-    polygon_area = Point(-79.9837,40.4273).buffer(0.0015)
-
-  return polygon_area
+def get_region(region: str) -> Polygon:
+  try:
+    return REGION_POLYGONS[region]
+  except:
+    raise ValueError(
+      f'Unsupported region {region}.'
+      'Please choose one of ', ALLOWED_REGIONS)
