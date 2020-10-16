1fdb7781c7f8bdfa54f7b4452abed822d1dc9971
Jason Baldridge <jasonbaldridge@google.com>
2020-10-16 17:38:18 +0000

    Output goes to ./tmp instead of /tmp to avoid permission errors.

--- run_all.sh
+++ run_all.sh
@@ -1,17 +1,18 @@
 REGION_NAME="Pittsburgh_small"
+OUTPUT_DIR='./tmp/cabby_run'
 
 echo "****************************************"
 echo "*                 Geo                  *"
 echo "****************************************"
-rm -rf ~/tmp/cabby_output/poiTestData
-mkdir -p ~/tmp/cabby_output/poiTestData
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR/poiTestData
 
 # Do we still need geo_computation?
 # bazel-bin/cabby/geo/geo_computation --orig_lat 40.749102 --orig_lon -73.984076 -dest_lat 40.748432 --dest_lon -73.982473
 
-bazel-bin/cabby/geo/map_processing/map_processor --region $REGION_NAME --min_s2_level 18 --directory "~/tmp/cabby_output/poiTestData/"
-bazel-bin/cabby/geo/map_processing/map_processor --region DC --min_s2_level 18 --directory "~/tmp/cabby_output/poiTestData/"
-bazel-bin/cabby/geo/sample_poi --region DC --min_s2_level 18 --directory "~/tmp/cabby_output/poiTestData/" --path "~/tmp/cabby_output/pathData/dc_geo_paths.gpkg" --n_samples 1
+bazel-bin/cabby/geo/map_processing/map_processor --region $REGION_NAME --min_s2_level 18 --directory $OUTPUT_DIR/poiTestData/
+bazel-bin/cabby/geo/map_processing/map_processor --region DC --min_s2_level 18 --directory $OUTPUT_DIR/poiTestData
+bazel-bin/cabby/geo/sample_poi --region DC --min_s2_level 18 --directory $OUTPUT_DIR/poiTestData --path $OUTPUT_DIR/pathData/dc_geo_paths.gpkg --n_samples 1
 
 echo "****************************************"
 echo "*              Wikidata                *"
@@ -28,9 +29,9 @@ echo "****************************************"
 echo "*              Wikigeo                 *"
 echo "****************************************"
 bazel-bin/cabby/data/extract_wikigeo_contexts --region $REGION_NAME --output_dir /tmp/wikigeo
-bazel-bin/cabby/data/extract_wikigeo_contexts --region $REGION_NAME --output_dir /tmp/wikigeo --osm_path "~/tmp/cabby_output/poiTestData/pittsburgh_small_poi.pkl"
+bazel-bin/cabby/data/extract_wikigeo_contexts --region $REGION_NAME --output_dir /tmp/wikigeo --osm_path $OUTPUT_DIR/poiTestData/pittsburgh_small_poi.pkl
 
 echo "****************************************"
 echo "*                 RVS                  *"
 echo "****************************************"
-bazel-bin/cabby/rvs/generate_rvs --rvs_data_path "~/tmp/cabby_output/pittsburgh_small_geo_paths.gpkg" --save_instruction_path "~/tmp/cabby_output/pittsburgh_small_instructions.json" 
+bazel-bin/cabby/rvs/generate_rvs --rvs_data_path $OUTPUT_DIR/pittsburgh_small_geo_paths.gpkg --save_instruction_path $OUTPUT_DIR/pittsburgh_small_instructions.json
