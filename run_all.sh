37e9679b1a49a9f403ea53ea493bdcb230a655a6
Jason Baldridge <jasonbaldridge@google.com>
2020-10-16 15:29:16 +0000

    Reorganized run_all.sh and am working to make it so it doesn't rely on existing pregenerated data files.

--- run_all.sh
+++ run_all.sh
@@ -1,18 +1,34 @@
-# Geo
-bazel-bin/cabby/geo/geo_computation --orig_lat 40.749102 --orig_lon -73.984076 -dest_lat 40.748432 --dest_lon -73.982473
-bazel-bin/cabby/geo/map_processing/map_processor --region DC --min_s2_level 18 --directory "./cabby/geo/map_processing/poiTestData/"
-bazel-bin/cabby/geo/map_processing/map_processor --region Pittsburgh_small --min_s2_level 18 --directory "./cabby/geo/map_processing/poiTestData/"
-bazel-bin/cabby/geo/sample_poi --region DC --min_s2_level 18 --directory "./cabby/geo/map_processing/poiTestData/" --path "./cabby/geo/pathData/dc_geo_paths.gpkg" --n_samples 1
+REGION_NAME="Pittsburgh_small"
 
-# RVS
-bazel-bin/cabby/rvs/generate_rvs --rvs_data_path "./cabby/geo/pathData/pittsburgh_geo_paths.gpkg" --save_instruction_path "./cabby/rvs/data/pittsburgh_instructions.json" 
+echo "****************************************"
+echo "*                 Geo                  *"
+echo "****************************************"
+mkdir -p /tmp/poiTestData
 
-# Wikidata
-bazel-bin/cabby/data/wikidata/extract_geofenced_wikidata_items --region Pittsburgh
+# Do we still need geo_computation?
+# bazel-bin/cabby/geo/geo_computation --orig_lat 40.749102 --orig_lon -73.984076 -dest_lat 40.748432 --dest_lon -73.982473
+
+bazel-bin/cabby/geo/map_processing/map_processor --region $REGION_NAME --min_s2_level 18 --directory "/tmp/poiTestData/"
+bazel-bin/cabby/geo/sample_poi --region $REGION_NAME --min_s2_level 18 --directory "/tmp/poiTestData/" --path "/tmp/pathData/pittburgh_small_geo_paths.gpkg" --n_samples 1
+
+echo "****************************************"
+echo "*              Wikidata                *"
+echo "****************************************"
+bazel-bin/cabby/data/wikidata/extract_geofenced_wikidata_items --region $REGION_NAME
 
 # Wikipedia
+echo "****************************************"
+echo "*              Wikipedia               *"
+echo "****************************************"
 bazel-bin/cabby/data/wikipedia/extract_wikipedia_items --titles=New_York_Stock_Exchange,Empire_State_Building
 
-# Wikigeo
-bazel-bin/cabby/data/extract_wikigeo_contexts --region Pittsburgh_small --output_dir /tmp/wikigeo
-bazel-bin/cabby/data/extract_wikigeo_contexts --region Pittsburgh_small --output_dir /tmp/wikigeo --osm_path  "./cabby/geo/map_processing/poiTestData/pittsburgh_small_poi.pkl"
+echo "****************************************"
+echo "*              Wikigeo                 *"
+echo "****************************************"
+bazel-bin/cabby/data/extract_wikigeo_contexts --region $REGION_NAME --output_dir /tmp/wikigeo
+bazel-bin/cabby/data/extract_wikigeo_contexts --region $REGION_NAME --output_dir /tmp/wikigeo --osm_path  "/tmp/poiTestData/pittsburgh_small_poi.pkl"
+
+echo "****************************************"
+echo "*                 RVS                  *"
+echo "****************************************"
+bazel-bin/cabby/rvs/generate_rvs --rvs_data_path "/tmp/pittsburgh_small_geo_paths.gpkg" --save_instruction_path "/tmp/pittsburgh_small_instructions.json" 
