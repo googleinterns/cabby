e629a032f65c8ccd1b2267463c85ddcfe408dfef
tzufgoogle <tzuf@google.com>
2020-10-18 14:24:51 +0000

    nit

--- cabby/model/text/dual_encoder/dataset.py
+++ cabby/model/text/dual_encoder/dataset.py
@@ -149,11 +149,11 @@ def create_dataset(data_dir: Text, region: Text, s2level: int
   tens_cells = torch.tensor(vec_cells)
 
   # Create Cabby dataset.
-  path = os.path(data_dir, 'train')
+  path = os.path.join(data_dir, 'train')
   train_dataset = TextGeoSplit(train_ds, s2level, cells, cellid_to_label, path)
-  path = os.path(data_dir, 'valid')
+  path = os.path.join(data_dir, 'valid')
   val_dataset = TextGeoSplit(valid_ds, s2level, cells, cellid_to_label, path)
-  path = os.path(data_dir, 'test')
+  path = os.path.join(data_dir, 'test')
   test_dataset = TextGeoSplit(test_ds, s2level, cells, cellid_to_label, path)
 
   return dataset_item.TextGeoDataset.from_TextGeoSplit(
