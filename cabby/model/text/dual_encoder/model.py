f934461c65f8b15582cd8b587c90de1436412589
tzufgoogle <tzuf@google.com>
2020-10-19 09:02:27 +0000

    fix call to transformer

--- cabby/model/text/dual_encoder/model.py
+++ cabby/model/text/dual_encoder/model.py
@@ -50,6 +50,7 @@ class DualEncoder(nn.Module):
 
   def text_embed(self, text):
     outputs = self.transformer(inputs_embeds=text)
-    last_hidden_state = outputs[1]
+    print (outputs)
+    last_hidden_state = outputs[0]
     cls_token = last_hidden_state[:,-1,:]
     return self.text_main(cls_token)
