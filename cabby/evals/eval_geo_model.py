787462e68a22c37840b8ced3d09cd8fc7c181d43
tzufgoogle <tzuf@google.com>
2020-10-22 16:20:33 +0000

    fixed build

--- cabby/evals/eval_geo_model.py
+++ cabby/evals/eval_geo_model.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Utility for distance based evaluation for geo models in RVS.
-
 Example:
 $ bazel run -- cabby/evals/eval_geo_model \
   --input_file /mnt/hackney/data/cabby/evals/sample_predictions.tsv
@@ -23,15 +22,6 @@ The input_file is expected to be a tab-separated file with:
   predicted_latitude
   predicted_longitude
 Any lines which do not comply to this format will be skipped.
-
-Alternately it can read from a pytorch state dictionaries with:
-  true_points_list
-  pred_points_list
-
-$ bazel run -- cabby/evals/eval_geo_model \
-  --input_file /mnt/hackney/data/cabby/evals/metrics.pt \
-  --preprocess_pytorch_output=true
-
 """
 
 from absl import app, flags
@@ -40,7 +30,7 @@ import torch
 
 from cabby import logger
 from cabby.evals import utils
-from cabby.model.text.s2cellid_prediction import train
+from cabby.model.text import util
 
 FLAGS = flags.FLAGS
 flags.DEFINE_bool("preprocess_pytorch_output", False,
@@ -55,24 +45,24 @@ def main(argv):
   del argv  # Unused
   eval_logger = logger.create_logger("rvs_geo_eval.log", "rvs_geo_eval")
   tsv_input = FLAGS.input_file
-  if FLAGS.preprocess_pytorch_output:
-    eval_logger.info("Preprocessing the pytorch output")
-    state_dict = train.load_metrics(
-      FLAGS.input_file, torch.device("cpu", 0))
-    eval_logger.info("Keys in state dictionary : [%s]", state_dict.keys())
+  # if FLAGS.preprocess_pytorch_output:
+  #   eval_logger.info("Preprocessing the pytorch output")
+  #   state_dict = util.load_metrics(
+  #     FLAGS.input_file, torch.device("cpu", 0))
+  #   eval_logger.info("Keys in state dictionary : [%s]", state_dict.keys())
 
-    output_file = FLAGS.input_file + ".raw_data.tsv"
-    fout = open(output_file, "w")
+  #   output_file = FLAGS.input_file + ".raw_data.tsv"
+  #   fout = open(output_file, "w")
 
-    for cnt, (true, pred) in enumerate(zip(state_dict['true_points_list'][0], state_dict['pred_points_list'][0])):
-      fout.write("%d\t%f\t%f\t%f\t%f\n" %
-             (cnt, true[0], true[1], pred[0], pred[1]))
-      if cnt % 1000:
-        eval_logger.info("Read [%d] examples", cnt)
-    fout.close()
-    eval_logger.info(
-      "Intermediate raw data TSV is generated in : [%s]", output_file)
-    tsv_input = output_file
+  #   for cnt, (true, pred) in enumerate(zip(state_dict['true_points_list'][-1], state_dict['pred_points_list'][-1])):
+  #     fout.write("%d\t%f\t%f\t%f\t%f\n" %
+  #            (cnt, true[0], true[1], pred[0], pred[1]))
+  #     if cnt % 1000:
+  #       eval_logger.info("Read [%d] examples", cnt)
+  #   fout.close()
+  #   eval_logger.info(
+  #     "Intermediate raw data TSV is generated in : [%s]", output_file)
+  #   tsv_input = output_file
 
   evaluator = utils.Evaluator()
   error_distances = evaluator.get_error_distances(tsv_input)
